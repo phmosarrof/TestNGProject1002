e61e0e0ff98302e4eb2d4a578be72267902fe14a
Mosarrof Hossain <phmosharaf@gmail.com>
2018-09-12 14:19:38 -0400

    code updated

---
+++ src/testNGProject/TestNGProject01.java
@@ -0,0 +1,34 @@
+package testNGProject;
+
+import org.testng.annotations.Test;
+
+public class TestNGProject01 {
+
+	//public static void main(String[] args) {
+
+	@Test
+	public void testCase10() {
+		System.out.println("\nThe Test is run by TestNG Framework - Test10");
+
+	}
+	
+	@Test
+	public void testCase11() {
+		System.out.println("\nThe Test is run by TestNG Framework - Test11");
+
+	}
+	@Test
+	public void testCase12() {
+		System.out.println("\nThe Test is run by TestNG Framework - Test12");
+	
+	}
+	@Test
+	public void testCase13() {
+		System.out.println("\nThe Test is run by TestNG Framework - Test13");
+
+	}
+	
+	
+	}
+
+
