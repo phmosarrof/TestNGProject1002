0363770e345784ce93b1f1a474dd3014a44c1da0
Mosarrof Hossain <phmosharaf@gmail.com>
2018-09-12 13:08:17 -0400

    TestNG Code added

---
+++ src/testNGProject/TestNGProject.java
@@ -0,0 +1,34 @@
+package testNGProject;
+
+import org.testng.annotations.Test;
+
+public class TestNGProject {
+
+	//public static void main(String[] args) {
+
+	@Test
+	public void testCase06() {
+		System.out.println("\nThe Test is run by TestNG Framework - Test06.");
+
+	}
+	
+	@Test
+	public void testCase07() {
+		System.out.println("\nThe Test is run by TestNG Framework - Test07.");
+
+	}
+	@Test
+	public void testCase08() {
+		System.out.println("\nThe Test is run by TestNG Framework - Test08.");
+	
+	}
+	@Test
+	public void testCase09() {
+		System.out.println("\nThe Test is run by TestNG Framework - Test09.");
+
+	}
+	
+	
+	}
+
+
